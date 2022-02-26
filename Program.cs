07dbaac0327a504b83beca87142df3e6c6694115
Javier Chamo <francisco12102002cl@gmail.com>
2022-02-25 20:40:04 -0600

    entrada-salida

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class EntradaSalidad
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.Read();
+        }
+    }
+}
